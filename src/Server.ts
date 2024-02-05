9ceefcbf6b39d2e9a33f8d969db3067fc04063b4
VictoriqueMoe <loli@victorique.moe>
2024-02-05 15:08:12 +0000

    enable protection to test

--- src/Server.ts
+++ src/Server.ts
@@ -20,7 +20,9 @@ import cookieParser from "cookie-parser";
 import session from "express-session";
 import methodOverride from "method-override";
 import {isProduction} from "./config/envs/index.js";
+import helmet from "helmet";
 import process from "process";
+import cors from "cors";
 import {TypeormStore} from "connect-typeorm";
 import {SQLITE_DATA_SOURCE} from "./model/di/tokens.js";
 import {DataSource} from "typeorm";
@@ -100,7 +102,7 @@ const opts: Partial<TsED.Configuration> = {
         }
     },
     middlewares: [
-        /* helmet({
+        helmet({
             contentSecurityPolicy: false,
             crossOriginEmbedderPolicy: {
                 policy: "credentialless"
@@ -109,7 +111,7 @@ const opts: Partial<TsED.Configuration> = {
         cors({
             origin: process.env.BASE_URL,
             exposedHeaders: ["Location"]
-        }), */
+        }),
         cookieParser(),
         methodOverride(),
         bodyParser.json(),
