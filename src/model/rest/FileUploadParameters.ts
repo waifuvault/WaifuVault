ac69f90e55540fab7a9dcf0944bb0af1bc8bc8a3
Walker Aldridge <daldridge@lairdscomputer.com>
2024-03-21 09:45:40 -0500

    Mark parameters optional

--- src/model/rest/FileUploadParameters.ts
+++ src/model/rest/FileUploadParameters.ts
@@ -1,4 +1,4 @@
-import { Description, Name, Pattern, Property } from "@tsed/schema";
+import { Description, Name, Optional, Pattern, Property } from "@tsed/schema";
 
 @Name("WaifuUploadParameters")
 @Description("Upload parameters for put requests")
@@ -7,22 +7,26 @@ export class FileUploadParameters {
         "a string containing a number and a letter of `m` for mins, `h` for hours, `d` for days. For example: `1h` would be 1 hour and `1d` would be 1 day. leave this blank if you want the file to exist according to the retention policy",
     )
     @Pattern(/^$|^\d+[mhd]$/)
+    @Optional()
     @Property()
     public expires?: string;
 
     @Description(
         "if set to true, then your filename will not appear in the URL. if false, then it will appear in the URL. defaults to false",
     )
+    @Optional()
     @Property()
     public hide_filename?: boolean;
 
     @Description(
         "Set a password for this file, this will encrypt the file on the server that not even the server owner can obtain it, when fetching the file. you can fill out the `x-password` http header with your password to obtain the file via API",
     )
+    @Optional()
     @Property()
     public password?: string;
 
     @Description("Shh, it's a secret ;)")
+    @Optional()
     @Property()
     public secret_token?: string;
 }
