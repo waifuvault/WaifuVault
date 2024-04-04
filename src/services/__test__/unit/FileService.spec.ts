b344dac3d51c0992d0be6ca71e7060eeac0bd386
Walker Aldridge <daldridge@lairdscomputer.com>
2024-04-04 18:09:58 -0500

    File Service Skeleton

---
+++ src/services/__test__/unit/FileService.spec.ts
@@ -0,0 +1,114 @@
+import { afterEach, beforeEach, describe, it, vi } from "vitest";
+import { platformCreate, setUpDataSource } from "../../../__test__/testUtils.spec.js";
+import { PlatformTest } from "@tsed/common";
+import { FileService } from "../../FileService.js";
+
+describe("unit tests", () => {
+    beforeEach(async () => {
+        await platformCreate();
+        setUpDataSource();
+    });
+
+    afterEach(async () => {
+        vi.clearAllMocks();
+        await PlatformTest.reset();
+    });
+
+    describe("processUpload", () => {
+        it(
+            "should process a given upload",
+            PlatformTest.inject([FileService], async (fileService: FileService) => {
+                // given
+
+                // when
+                await fileService.getFileInfo("sometoken", true);
+
+                // then
+            }),
+        );
+    });
+
+    describe("isFileEncrypted", () => {
+        it(
+            "should return a bool with encryption state for a given file",
+            PlatformTest.inject([FileService], async (fileService: FileService) => {
+                // given
+
+                // when
+                await fileService.getFileInfo("sometoken", true);
+
+                // then
+            }),
+        );
+    });
+
+    describe("requiresPassword", () => {
+        it(
+            "should return true if entry requires a password",
+            PlatformTest.inject([FileService], async (fileService: FileService) => {
+                // given
+
+                // when
+                await fileService.getFileInfo("sometoken", true);
+
+                // then
+            }),
+        );
+    });
+
+    describe("getEntry", () => {
+        it(
+            "should return a decrypted buffer and fileupload model for a given filename",
+            PlatformTest.inject([FileService], async (fileService: FileService) => {
+                // given
+
+                // when
+                await fileService.getFileInfo("sometoken", true);
+
+                // then
+            }),
+        );
+    });
+
+    describe("modifyEntry", () => {
+        it(
+            "should modify an entry based on a given modification dto and token",
+            PlatformTest.inject([FileService], async (fileService: FileService) => {
+                // given
+
+                // when
+                await fileService.getFileInfo("sometoken", true);
+
+                // then
+            }),
+        );
+    });
+
+    describe("getFileInfo", () => {
+        it(
+            "should return file information for a given token and human readable bool",
+            PlatformTest.inject([FileService], async (fileService: FileService) => {
+                // given
+
+                // when
+                await fileService.getFileInfo("sometoken", true);
+
+                // then
+            }),
+        );
+    });
+
+    describe("processDelete", () => {
+        it(
+            "should delete the files for a given list of tokens",
+            PlatformTest.inject([FileService], async (fileService: FileService) => {
+                // given
+
+                // when
+                await fileService.getFileInfo("sometoken", true);
+
+                // then
+            }),
+        );
+    });
+});
