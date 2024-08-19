ab09e64fdc726b8f192170677121153f80371027
VictoriqueMoe <loli@victorique.moe>
2024-08-19 12:05:11 +0100

    ui changes

--- src/controllers/secure/BucketView.ts
+++ src/controllers/secure/BucketView.ts
@@ -1,19 +1,24 @@
-import { Controller } from "@tsed/di";
+import { Controller, Inject } from "@tsed/di";
 import { Get, Hidden, View } from "@tsed/schema";
 import { Authorize } from "@tsed/passport";
 import type { CustomUserInfoModel } from "../../model/auth/CustomUserInfoModel";
 import { Req } from "@tsed/common";
 import type { Request } from "express";
+import { BucketService } from "../../services/BucketService.js";
 
 @Controller("/bucket")
 @Hidden()
 export class BucketView {
+    public constructor(@Inject() private bucketService: BucketService) {}
+
     @Get()
     @View("/secure/files.ejs")
     @Authorize("bucketAuthProvider")
-    public showBucketPage(@Req() req: Request): unknown {
+    public async showBucketPage(@Req() req: Request): Promise<unknown> {
+        const bucket = await this.bucketService.getBucket();
         return {
             user: req.user as CustomUserInfoModel,
+            bucket,
         };
     }
 }
