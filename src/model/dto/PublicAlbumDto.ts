23fafb2fd4b194a9bc1824ba1c5549f994b129d3
VictoriqueMoe <loli@victorique.moe>
2025-01-12 22:48:58 +0000

    enrich public dto with thumbnail

--- src/model/dto/PublicAlbumDto.ts
+++ src/model/dto/PublicAlbumDto.ts
@@ -2,6 +2,15 @@ import { CollectionOf, Description, Name, Property } from "@tsed/schema";
 import { FileUploadResponseDto } from "./FileUploadResponseDto.js";
 import { AlbumModel } from "../db/Album.model.js";
 import { Builder } from "builder-pattern";
+import { FileUtils } from "../../utils/Utils.js";
+import { constant } from "@tsed/di";
+import GlobalEnv from "../constants/GlobalEnv.js";
+import { FileUploadModel } from "../db/FileUpload.model.js";
+
+type FileMetadata = {
+    thumbnail: string | null;
+    mediaType: string | null;
+};
 
 @Name("WaifuPublicAlbum")
 @Description("A public album is a shared collection of files, it can be viewed by anyone")
@@ -21,21 +30,35 @@ export class PublicAlbumDto {
         size: number;
         protected: boolean;
         id: number;
+        metadata: FileMetadata;
     }[];
 
     public static fromModel(model: AlbumModel): PublicAlbumDto {
         const fileDtos = model.files
             ? model.files.map(f => {
                   const { url, options } = FileUploadResponseDto.fromModel(f, true, false);
+                  const metadata: FileMetadata = {
+                      thumbnail: PublicAlbumDto.getThumbnail(model, f),
+                      mediaType: f.mediaType,
+                  };
                   return {
                       url,
                       protected: options?.protected ?? false,
                       name: f.parsedFileName,
                       size: f.fileSize,
                       id: f.id,
+                      metadata,
                   };
               })
             : [];
         return Builder(PublicAlbumDto).name(model.name).files(fileDtos).build();
+    }
+
+    private static getThumbnail(album: AlbumModel, file: FileUploadModel): string | null {
+        if (!FileUtils.isImage(file)) {
+            return null;
+        }
+        const baseUrl = constant(GlobalEnv.BASE_URL) as string;
+        return `${baseUrl}/rest/album/operations/${album.publicToken}/thumbnail?imageId=${file.id}`;
     }
 }
