34b49ec2a5e575c237e453db767bcdb8331b3f49
Walker Aldridge <daldridge@lairdscomputer.com>
2025-01-24 10:23:12 -0600

    Remove unused context

--- src/engine/impl/transfomers/dto/StatsModelTransformer.ts
+++ src/engine/impl/transfomers/dto/StatsModelTransformer.ts
@@ -1,7 +1,6 @@
-import { Injectable, InjectContext, ProviderScope } from "@tsed/di";
+import { Injectable, ProviderScope } from "@tsed/di";
 import { TRANSFORMER } from "../../../../model/di/tokens.js";
 import type { ITransformer } from "../../../ITransformer.js";
-import type { PlatformContext } from "@tsed/common";
 import { StatsDto, StatsModel } from "../../../../model/dto/StatsDto.js";
 import { AdminFileData } from "../../../../model/dto/AdminData.js";
 
@@ -10,9 +9,6 @@ import { AdminFileData } from "../../../../model/dto/AdminData.js";
     type: TRANSFORMER,
 })
 export class StatsModelTransformer implements ITransformer<StatsModel, StatsDto> {
-    @InjectContext()
-    private $ctx?: PlatformContext;
-
     public supportsInput(input: unknown): boolean {
         return input instanceof StatsModel;
     }
