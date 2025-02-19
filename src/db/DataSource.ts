9b2a2f5b5c5c33e6fbb7026915901ba3823efb46
VictoriqueMoe <loli@victorique.moe>
2025-02-19 13:26:34 +0000

    fix build

--- src/db/DataSource.ts
+++ src/db/DataSource.ts
@@ -1,4 +1,4 @@
-import { DataSource } from "typeorm";
+import { DataSource, type DataSourceOptions } from "typeorm";
 import path from "node:path";
 import { fileURLToPath } from "node:url";
 import process from "process";
@@ -6,18 +6,28 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-const url = new URL(process.env.REDIS_URI as string);
-export const dataSource = new DataSource({
+const argv = process.argv.slice(2);
+
+let redisUrl: URL | undefined;
+if (!argv.includes("-closeOnStart")) {
+    redisUrl = new URL(process.env.REDIS_URI as string);
+}
+
+const options: DataSourceOptions = {
     type: "better-sqlite3",
     entities: [`${path.dirname(fileURLToPath(import.meta.url))}/../model/db/**/*.model.{ts,js}`],
     synchronize: false,
     migrations: [`${path.dirname(fileURLToPath(import.meta.url))}/../migrations/*`],
     database: "main.sqlite",
-    cache: {
-        type: "ioredis",
-        options: {
-            host: url.hostname,
-            port: Number.parseInt(url.port),
-        },
-    },
-});
+    cache: argv.includes("-closeOnStart")
+        ? false
+        : {
+              type: "ioredis",
+              options: {
+                  host: redisUrl!.hostname,
+                  port: Number.parseInt(redisUrl!.port),
+              },
+          },
+};
+
+export const dataSource = new DataSource(options as DataSourceOptions);
