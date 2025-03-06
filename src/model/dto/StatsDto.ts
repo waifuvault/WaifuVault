64f89768c21b343a32a9c8cb914b21ae27f3680c
Walker Aldridge <daldridge@lairdscomputer.com>
2025-03-06 16:57:50 -0600

    Add stat counts for buckets and albums

--- src/model/dto/StatsDto.ts
+++ src/model/dto/StatsDto.ts
@@ -15,16 +15,49 @@ export class StatsDto {
     public totalFileSize: number;
 
     @Property()
+    public totalBuckets: number;
+
+    @Property()
+    public averageBucketSize: number;
+
+    @Property()
+    public totalAlbums: number;
+
+    @Property()
+    public averageAlbumSize: number;
+
+    @Property()
     @CollectionOf(AdminFileData)
     public entries: AdminFileData[];
 
     public static async buildStats(entries: AdminFileData[]): Promise<StatsDto> {
         const realFiles = await FileUtils.getFilesCount();
         const fileSizes = entries.reduce((acc, currentValue) => acc + currentValue.fileSize, 0);
+        const bucketSet = entries
+            .map(e => e.bucket)
+            .reduce((set, item) => {
+                set.add(item ?? "");
+                return set;
+            }, new Set<string>());
+        bucketSet.delete("");
+        const bucketSizes = entries.filter(e => e.bucket).reduce((acc, currentValue) => acc + currentValue.fileSize, 0);
+        const albumSet = entries
+            .map(e => e.album)
+            .reduce((set, item) => {
+                set.add(item?.token ?? "");
+                return set;
+            }, new Set<string>());
+        albumSet.delete("");
+        const albumSizes = entries.filter(e => e.album).reduce((acc, currentValue) => acc + currentValue.fileSize, 0);
+
         const statsBuilder = Builder(StatsDto)
             .totalFileCount(entries.length)
             .realFileCount(realFiles)
             .totalFileSize(fileSizes)
+            .totalBuckets(bucketSet.size)
+            .averageBucketSize(Math.floor(bucketSizes / bucketSet.size))
+            .totalAlbums(albumSet.size)
+            .averageAlbumSize(Math.floor(albumSizes / albumSet.size))
             .entries(entries);
         return statsBuilder.build();
     }
