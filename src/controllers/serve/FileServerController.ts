322a3929ba465198b1e7ef85df35c570b9fe683c
VictoriqueMoe <loli@victorique.moe>
2025-07-06 18:13:57 +0100

    end my suffering x3

--- src/controllers/serve/FileServerController.ts
+++ src/controllers/serve/FileServerController.ts
@@ -22,7 +22,7 @@ export class FileServerController {
     private readonly allowedChunkMimeTypes = ["video/", "audio/"];
     private readonly chunkSize = 10 ** 6; // 1MB
 
-    @Get("/:t/*file")
+    @Get("/:t/{:file}")
     @Header({
         "Content-Encoding": "identity", // disable gzip
     })
