248f2fc127222d4bb16eed8e8521ee408910b4de
Victoria <loli@victorique.moe>
2025-08-19 21:49:09 +0100

    dump deps and add some small changes to home page (#296)
    * dump deps and add some small changes to home page
    
    * Add walker
    
    ---------
    
    Co-authored-by: Walker Aldridge <daldridge@lairdscomputer.com>

--- src/config/logger/index.ts
+++ src/config/logger/index.ts
@@ -1,23 +1,25 @@
-import { type DILoggerOptions } from "@tsed/di";
 import { $log } from "@tsed/logger";
 import { isProduction } from "../envs/index.js";
 import process from "process";
+import { DILoggerOptions } from "@tsed/di";
+import "@tsed/logger-std";
+import "@tsed/logger/layouts/JsonLayout.js";
 
 if (isProduction) {
-    /*    $log.appenders.set("stdout", {
+    $log.appenders.set("stdout", {
         type: "stdout",
         levels: ["info", "debug"],
         layout: {
             type: "json",
         },
-    });*/
-    /*    $log.appenders.set("stderr", {
+    });
+    $log.appenders.set("stderr", {
         levels: ["trace", "fatal", "error", "warn"],
         type: "stderr",
         layout: {
             type: "json",
         },
-    });*/
+    });
 }
 
 $log.level = getLogLevelsByEnv();
