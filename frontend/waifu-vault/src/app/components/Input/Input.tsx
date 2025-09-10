46588f61cfaced6b5acb8e3ebb4491b23f27ba3c
VictoriqueMoe <loli@victorique.moe>
2025-09-10 19:38:59 +0100

    do not use old forwardRef

--- frontend/waifu-vault/src/app/components/Input/Input.tsx
+++ frontend/waifu-vault/src/app/components/Input/Input.tsx
@@ -1,23 +1,20 @@
 "use client";
 
-import { forwardRef, InputHTMLAttributes } from "react";
+import { InputHTMLAttributes, Ref } from "react";
 import styles from "./Input.module.scss";
 
 export interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
     variant?: "default" | "search";
     error?: boolean;
+    ref?: Ref<HTMLInputElement>;
 }
 
-export const Input = forwardRef<HTMLInputElement, InputProps>(
-    ({ className, variant = "default", error = false, ...props }, ref) => {
-        return (
-            <input
-                className={`${styles.input} ${styles[variant]} ${error ? styles.error : ""} ${className || ""}`}
-                ref={ref}
-                {...props}
-            />
-        );
-    },
-);
-
-Input.displayName = "Input";
+export const Input = ({ className, variant = "default", error = false, ref, ...props }: InputProps) => {
+    return (
+        <input
+            className={`${styles.input} ${styles[variant]} ${error ? styles.error : ""} ${className || ""}`}
+            ref={ref}
+            {...props}
+        />
+    );
+};
